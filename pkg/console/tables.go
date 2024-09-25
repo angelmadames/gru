bbcc9d83dbbae408549158cf4f93d2eef996d35f
Angel M. Adames <angelmadames@gmail.com>
2024-09-24 21:05:51 -0400

    chore: Cleanup styling

--- pkg/console/tables.go
+++ pkg/console/tables.go
@@ -5,6 +5,7 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/table"
 	"github.com/google/go-github/v65/github"
+	"time"
 )
 
 type ReposTableData struct {
@@ -12,13 +13,34 @@ type ReposTableData struct {
 }
 
 func ReposTable(data ReposTableData) {
-	t := table.New().
-		Border(lipgloss.NormalBorder()).
-		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("20"))).
-		Headers("NAME", "DEFAULT BRANCH")
+	t := table.New()
+	borderStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("99"))
+	headerStyle := lipgloss.NewStyle().PaddingLeft(2).PaddingRight(2).Bold(true)
+	rowStyle := lipgloss.NewStyle().PaddingLeft(2).PaddingRight(2)
+
+	t.Border(lipgloss.NormalBorder())
+	t.Width(80)
+	t.StyleFunc(func(row, col int) lipgloss.Style {
+		switch {
+		case row == 0:
+			return headerStyle
+		default:
+			return rowStyle
+		}
+	})
+	t.BorderStyle(borderStyle)
+	t.Headers(
+		"NAME",
+		"VISIBILITY",
+		"LAST UPDATED",
+	)
 
 	for _, repo := range data.Repositories {
-		t.Row(repo.GetName(), repo.GetDefaultBranch())
+		t.Row(
+			repo.GetName(),
+			repo.GetVisibility(),
+			repo.GetUpdatedAt().Format(time.RFC1123Z),
+		)
 	}
 
 	fmt.Println(t)
